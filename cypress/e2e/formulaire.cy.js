15f4deb402ae0c86032f3da30a25cbba29d8a9d3
marceau07 <marceau0707@gmail.com>
2025-02-27 12:14:28 +0100

    Changement du lien de test

--- cypress/e2e/formulaire.cy.js
+++ cypress/e2e/formulaire.cy.js
@@ -3,7 +3,7 @@ describe('Test formulaire d\'ajout', () => {
     cy.visit('https://testing.adrardev.fr/addUser')
     cy.get('input[name="nom"]').type('DOE')
     cy.get('input[name="prenom"]').type('John')
-    cy.get('input[name="mail"]').type('')
+    cy.get('input[name="mail"]').type(Math.random().toString(36).substring(2, 15) + '@gmail.com')
     cy.get('input[name="mdp"]').type('!P4sSw0rD!')
     cy.get('input[type="submit"]').click()
     cy.get('#msgzone').should('contain', "Le compte a été ajouté en BDD")
